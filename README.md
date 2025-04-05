03c9ec4265653e7d64b4d379478eeeb99cbbbe57
Filip Jaros <fjaros91@gmail.com>
2025-04-05 09:38:11 -0700

    Rewrite for anniversary realms

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ Raid Reset Tracker -- README
 =================
 Raid Reset Tracker is a World of Warcraft Classic Addon to extend your Raid Info information to include reset times for raids to which you are not yet saved.
 
-Written by Munchkin \<clean\> on Fairbanks-US.
+Written by Buttercup \<Ironforged\> on Nightslayer-US.
 
 ![example](https://raw.githubusercontent.com/fjaros/raidresettracker/master/example.png)
--- RaidResetTracker.lua
+++ RaidResetTracker.lua
@@ -1,108 +1,98 @@
 
--- Only works with Burning Crusade Classic WoW
-if WOW_PROJECT_ID ~= WOW_PROJECT_BURNING_CRUSADE_CLASSIC then
+-- Only works with Classic WoW
+if WOW_PROJECT_ID ~= WOW_PROJECT_CLASSIC then
 	return
 end
-local VERSION = GetBuildInfo()
-local MAJOR, MINOR, PHASE = strsplit(".", VERSION)
-MAJOR = tonumber(MAJOR)
-MINOR = tonumber(MINOR)
-PHASE = tonumber(PHASE)
 
--- Due to localization, it's easier to keep track of all US/AUS/Oceanic realms
-local ALL_US_REALMS = {
-	"Anathema","Arcanite Reaper","Arugal","Ashkandi","Atiesh","Azuresong","Benediction","Bigglesworth","Blaumeux","Bloodsail Buccaneers","Deviate Delight","Earthfury","Faerlina","Fairbanks","Felstriker","Grobbulus","Heartseeker","Herod","Incendius","Kirtonos","Kromcrush","Kurinnaxx","Loatheb","Mankrik","Myzrael","Netherwind","Old Blanchy","Pagle","Rattlegore","Remulos","Skeram","Smolderweb","Stalagg","Sulfuras","Sul'thraze","Thalnos","Thunderfury","Westfall","Whitemane","Windseeker","Yojamba"
-}
-for _, k in ipairs(ALL_US_REALMS) do
-	ALL_US_REALMS[k] = true
-end
-
-local RESET_TIME_US = 1624374000
+local RESET_TIME_US = 1744124400
 local RESET_TIME_EU = RESET_TIME_US + 57600
+local NAME_ID_MAP = {
+	["Onyxia's Lair"] = 249,
+	["Zul'Gurub"] = 309,
+	["Molten Core"] = 409,
+	["Blackwing Lair"] = 469,
+	["Ruins of Ahn'Qiraj"] = 509,
+	["Ahn'Qiraj Temple"] = 531,
+	["Naxxramas"] = 533,
+}
 local RAID_ORDER = {}
+local SAVED_INSTANCES = {}
 
 local function addResetTime(id)
-	table.insert(RAID_ORDER, 1, id)
+	table.insert(RAID_ORDER, id)
 end
 
-if PHASE >= 1 then
-	addResetTime(544) -- Magtheridon's Lair
-	addResetTime(565) -- Gruul's Lair
-	addResetTime(532) -- Karazhan
-end
-if PHASE >= 2 then
-	addResetTime(548) -- Serpentshrine Cavern
-	addResetTime(550) -- Tempest Keep
-end
-if PHASE >= 3 then
-	addResetTime(534) -- The Battle for Mount Hyjal
-	addResetTime(564) -- The Black Temple
-end
-if PHASE >= 4 then
-	-- This reset time might need to change once we find out when it is during phase launch
-	addResetTime(568) -- Zul'Aman
-end
-if PHASE >= 5 then
-	addResetTime(580) -- The Sunwell
+addResetTime(249) -- Onyxia's Lair
+addResetTime(309) -- Zul'Gurub
+addResetTime(409) -- Molten Core
+addResetTime(469) -- Blackwing Lair
+addResetTime(509) -- Ruins of Ahn'Qiraj
+addResetTime(531) -- Ahn'Qiraj Temple
+addResetTime(533) -- Naxxramas
+
+local function is20(id)
+	return id == 309 or id == 509
 end
 
 local function getStartTime(id)
-	if ALL_US_REALMS[GetRealmName()] then
-		return RESET_TIME_US
+	local reset
+	if GetCurrentRegion() == 1 then
+		reset = RESET_TIME_US
 	else
-		if id == 568 then
-			return RESET_TIME_EU - 86400
-		else
-			return RESET_TIME_EU
-		end
+		reset = RESET_TIME_EU
 	end
+	if is20(id) then
+		reset = reset + 3600
+	end
+	return reset
 end
 
 local function getFreq(id)
-	local freq = id == 568 and 3 or 7
+	local freq
+	if is20(id) then
+		freq = 3
+	elseif id == 249 then
+		freq = 5
+	else
+		freq = 7
+	end
 	return freq * 86400
 end
 
 local _GetNumSavedInstances = _G.GetNumSavedInstances
 local _GetSavedInstanceInfo = _G.GetSavedInstanceInfo
-local resetState
 
 _G.GetNumSavedInstances = function()
-	resetState = {unpack(RAID_ORDER)}
-	local unsavedRaids = #resetState
-	local numRealSavedInstances = _GetNumSavedInstances()
-	for i = 1, numRealSavedInstances do
+	SAVED_INSTANCES = {}
+	for i = 1, _GetNumSavedInstances() do
 		local name = _GetSavedInstanceInfo(i)
-		for _, v in ipairs(resetState) do
-			if GetRealZoneText(v) == name then
-				unsavedRaids = unsavedRaids - 1
-				break
-			end
-		end
+		SAVED_INSTANCES[NAME_ID_MAP[name]] = i
 	end
-	return unsavedRaids + numRealSavedInstances
+	return #RAID_ORDER
 end
 
 _G.GetSavedInstanceInfo = function(index)
-	if index <= _GetNumSavedInstances() then
-		local name = _GetSavedInstanceInfo(index)
-		for i, v in ipairs(resetState) do
-			if GetRealZoneText(v) == name then
-				table.remove(resetState, i)
-				break
-			end
-		end
-		return _GetSavedInstanceInfo(index)
+	local id = RAID_ORDER[index]
+	if SAVED_INSTANCES[id] then
+		return _GetSavedInstanceInfo(SAVED_INSTANCES[id])
 	end
 
-	local id = resetState[index - _GetNumSavedInstances()]
-	if not id then
-		return _GetSavedInstanceInfo(index)
-	end
 	local epoch = GetServerTime()
 	local startTime = getStartTime(id)
 	local freq = getFreq(id)
 	local nextResetCoef = math.ceil((epoch - startTime) / freq)
 	local resetTime = startTime + (freq * nextResetCoef) - epoch
-	return GetRealZoneText(id), "NOT SAVED", resetTime, 0, false, false, 0, true
+
+	local difficultyId
+	local maxPlayers
+	if is20(id) then
+		difficultyId = 148
+		maxPlayers = 20
+	else
+		difficultyId = 9
+		maxPlayers = 40
+	end
+	local difficultyName = GetDifficultyInfo(difficultyId)
+
+	return GetRealZoneText(id), "NOT SAVED", resetTime, difficultyId, false, false, 0, true, maxPlayers, difficultyName, 0, 0, false, id
 end
